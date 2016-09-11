6151aa30d931fbd8b4f72ffde61964dfcee2c254
turkey <mathcyp2008@hotmail.com>
2016-09-11 18:12:01 +0800

    增加邮件发送模块

---
+++ sendmail.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import urllib
+import urllib2
+import smtplib,sys
+from email.mime.text import MIMEText
+
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+def send_mail(to_list,sub,content):
+    # 设置服务器，用户名，口令以及邮箱的后缀
+    mail_host="smtp.126.com"
+    mail_user="testinglife@126.com"
+    mail_pass="jianchi000"
+    msg = MIMEText(content,_subtype='plain',_charset='utf-8')
+    msg['Subject'] = sub
+    msg['From'] = mail_user
+    msg['To'] = ";".join(to_list)
+    try:
+        s = smtplib.SMTP()
+        s.connect(mail_host)
+        s.login(mail_user,mail_pass)
+        s.sendmail(mail_user, to_list, msg.as_string())
+        s.close()
+        return True
+    except Exception, e:
+        print str(e)
+        return False
+if __name__ == '__main__':
+    #date = sys.argv[1]
+    content = sys.argv[1]
+    #produceMsg()
+    mailto_list=["gsj_echo@163.com"]
+    mail_content = ""
+    with open(content, 'r') as f:
+        mail_content = f.read()
+    #send_mail(mailto_list, sub, content)
+    send_mail(mailto_list, u'每日预测', content)
